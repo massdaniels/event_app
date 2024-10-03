2e0be6a8fbc5257ff840ceb13092535c22dbc766
massdaniels <danielmsemakweli0@gmail.com>
2024-10-03 15:42:31 +0300

    fix

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,11 +4,11 @@ import { revalidatePath } from 'next/cache'
 
 import { connectToDatabase } from '@/lib/database'
 import User from '@/lib/database/models/user.model'
-// import Order from '@/lib/database/models/order.model'
 import Event from '@/lib/database/models/event.model'
 import { handleError } from '@/lib/utils'
 
 import { CreateUserParams, UpdateUserParams } from '@/types'
+import Order from '../database/models/order.model'
 
 export async function createUser(user: CreateUserParams) {
   try {
@@ -67,7 +67,7 @@ export async function deleteUser(clerkId: string) {
       ),
 
       // Update the 'orders' collection to remove references to the user
-     // Order.updateMany({ _id: { $in: userToDelete.orders } }, { $unset: { buyer: 1 } }),
+      Order.updateMany({ _id: { $in: userToDelete.orders } }, { $unset: { buyer: 1 } }),
     ])
 
     // Delete user
--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,19 +1,20 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.NONGODB_URI
+const MONGODB_URI = process.env.MONGODB_URI;
 
-const cached = (global as any ).mongoose || {conn: null, promise: null}
+let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
-    if (cached.conn) return cached.conn
+  if (cached.conn) return cached.conn;
 
-    if(!MONGODB_URI) throw new Error('MONGODB_URI IS NOT FOUND!')
+  if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-        dbName: 'EventExpress',
-        bufferCommands: false,
-    })
+  cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
+    dbName: 'eventexpdb',
+    bufferCommands: false,
+  })
 
-    cached.conn = await cached.promise
-    return cached.conn
+  cached.conn = await cached.promise;
+
+  return cached.conn;
 }
